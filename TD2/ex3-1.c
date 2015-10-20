a21da9c60cda02aca9fc8b1d46ca70101a95e1fd
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-10-20 15:55:55 +0200

    début de l’exercice 3

---
+++ TD2/ex3-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  for(int i = 0; i < argc; i++){
+    pid_t pid_commande1 = fork ();
+    switch (pid_commande1 ) {
+     case -1 : /* erreur */
+       perror ("\nErreur fork") ;
+     case 0 : /* fils */
+       printf ("fils : %d, pid du père : %d\n", getpid(), getppid());
+       printf ("mkdir :\n");
+       execlp(argv[i],argv[i], NULL);
+       exit(0); //On sort du switch case (pour ne pas passer par default)
+     default : /* pere */
+       printf ("pere : %d\n", getpid()) ;
+       wait(NULL); //On met le pere en pause pour l'empecher de se reproduire
+    }
+  }
+return 0;
+}
