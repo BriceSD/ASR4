4c773ad740a0e571bd260f8f58cfbd02116acd41
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-11-13 09:25:15 +0100

    Ajout de la ligne a afficher

--- SAUNIERDEBESBrice_TP1_ASR4/ex1.c
+++ SAUNIERDEBESBrice_TP1_ASR4/ex1.c
@@ -5,16 +5,23 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]){
- char *name= argv[1];
- int fd;
- char c;
+  char *name = argv[1];
+  int ligne_voulu = atoi(argv[2]);
+  int ligne_actuelle = 1;
+  int fd;
+  char c;
+  char caractere_fin_de_ligne = '\n';
 
-   if((fd = open(name, O_RDONLY))==-1){
-     perror("open");
-     exit(1);
-   }
+  if((fd = open(name, O_RDONLY))==-1){
+    perror("open");
+    exit(1);
+  }
 
   while(read(fd,&c,1)){
-   printf("%c", c);
+    if(c == caractere_fin_de_ligne)
+      ligne_actuelle++;
+    if(ligne_voulu == ligne_actuelle)
+      printf("%c", c);
+
   }
 }
